906dfaafa50c6efe9709d2c2a63670d216e9f4c8
RedSkyHarbor <PenelloJ2@gmail.com>
2016-11-06 10:37:45 -0500

    Added Tutors and their times

--- app/src/main/java/com/acm/njit/acm_tutoring/Sigs.java
+++ app/src/main/java/com/acm/njit/acm_tutoring/Sigs.java
@@ -1,26 +1,38 @@
 package com.acm.njit.acm_tutoring;
 
+import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Jesse on 11/5/2016.
- * Corresponds to sigs_layout.xml
+ * Corresponds to tutoring_layout.xml
  */
 
-public class Sigs extends Fragment {
-
-    View viewer;
-
-    @Nullable
+public class Sigs extends ListFragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        viewer = inflater.inflate(R.layout.sigs_layout, container, false);
-        return viewer;
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+        String[] sigs = getResources().getStringArray(R.array.sigs);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,sigs);
+
+        setListAdapter(dataAdapter);
+
 
     }
+
+
 }
