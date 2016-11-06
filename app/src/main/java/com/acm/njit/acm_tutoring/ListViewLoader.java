dadfd8f2df42f1608030cd7609a31592f3187a74
RedSkyHarbor <PenelloJ2@gmail.com>
2016-11-06 06:49:00 -0500

    Fixed menu

---
+++ app/src/main/java/com/acm/njit/acm_tutoring/ListViewLoader.java
@@ -0,0 +1,44 @@
+package com.acm.njit.acm_tutoring;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Jesse on 11/6/2016.
+ * For populating list view
+ */
+
+public class ListViewLoader extends Activity {
+
+/*    private ListView mainList;
+    private Adapter mainAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutoring_layout); //TODO tutoring_layout ??
+        initalizeListView();
+
+    }
+
+    private void initalizeListView(){
+        Log.d("Test", "Initislizing lidt view");
+        mainList = (ListView) findViewById(R.id.mainList);
+        mainAdapter = new Adapter(this);
+        mainList.setAdapter(mainAdapter);
+        mainAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }*/
+
+}
+
--- app/src/main/java/com/acm/njit/acm_tutoring/Tutoring.java
+++ app/src/main/java/com/acm/njit/acm_tutoring/Tutoring.java
@@ -3,9 +3,14 @@ package com.acm.njit.acm_tutoring;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Jesse on 11/5/2016.
@@ -15,12 +20,47 @@ import android.view.ViewGroup;
 public class Tutoring extends Fragment {
 
     View viewer;
+    private ListView mainList;
+    private Adapter mainAdapter;
+    private ArrayAdapter<String> itemsAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         viewer = inflater.inflate(R.layout.tutoring_layout, container, false);
+        initalizeListView(viewer);
+
         return viewer;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.tutoring_layout); //TODO tutoring_layout ??
+
 
     }
+
+    private void initalizeListView(View view){
+        Log.d("Test", "Initializing list view");
+        mainList = (ListView) view.findViewById(R.id.mainList);
+        mainAdapter = new Adapter(getActivity());
+        mainList.setAdapter(mainAdapter);
+        mainAdapter.notifyDataSetChanged();
+
+        ArrayList<String> tutorList = new ArrayList<String>();
+
+        /*itemsAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                tutorList
+        );*/
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+
 }
