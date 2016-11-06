793c9a8f395eac9b20e90ef70521d89301f8efeb
RedSkyHarbor <PenelloJ2@gmail.com>
2016-11-05 22:22:45 -0400

    Changed some stuff so prettier

--- app/src/main/java/com/acm/njit/acm_tutoring/MainActivity.java
+++ app/src/main/java/com/acm/njit/acm_tutoring/MainActivity.java
@@ -24,15 +24,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -84,15 +75,14 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_first_layout) {
-            manager.beginTransaction().replace(R.id.content_frame , new Tutoring()).commit();
+            manager.beginTransaction().replace(R.id.content_frame, new Tutoring()).commit();
         } else if (id == R.id.nav_second_layout) {
-            manager.beginTransaction().replace(R.id.content_frame , new Sigs()).commit();
+            manager.beginTransaction().replace(R.id.content_frame, new Sigs()).commit();
         } else if (id == R.id.nav_third_layout) {
-            manager.beginTransaction().replace(R.id.content_frame , new ThirdFragment()).commit();
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            manager.beginTransaction().replace(R.id.content_frame, new ThirdFragment()).commit();
+        } else if (id == R.id.nav_send)
+        {
+            //do SOMETHING eventually
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
