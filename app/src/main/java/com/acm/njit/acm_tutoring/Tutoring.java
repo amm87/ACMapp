531b574beacfd94197819570dd935be67819efd0
RedSkyHarbor <PenelloJ2@gmail.com>
2016-11-06 07:20:01 -0500

    Hopefully fix Tutoring.java

--- app/src/main/java/com/acm/njit/acm_tutoring/Tutoring.java
+++ app/src/main/java/com/acm/njit/acm_tutoring/Tutoring.java
@@ -1,5 +1,6 @@
 package com.acm.njit.acm_tutoring;
 
+import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,60 +8,49 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Jesse on 11/5/2016.
  * Corresponds to tutoring_layout.xml
  */
 
-public class Tutoring extends Fragment {
+public class Tutoring extends Activity {
 
-    View viewer;
+    //View viewer;
     private ListView mainList;
-    private Adapter mainAdapter;
-    private ArrayAdapter<String> itemsAdapter;
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        viewer = inflater.inflate(R.layout.tutoring_layout, container, false);
-        initalizeListView(viewer);
-
-        return viewer;
-    }
+    //private Adapter mainAdapter;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.tutoring_layout); //TODO tutoring_layout ??
+        setContentView(R.layout.tutoring_layout); //TODO ????
 
+        //Find ListView resource
+        mainList = (ListView) findViewById(R.id.mainList);
 
-    }
-
-    private void initalizeListView(View view){
-        Log.d("Test", "Initializing list view");
-        mainList = (ListView) view.findViewById(R.id.mainList);
-        mainAdapter = new Adapter(getActivity());
-        mainList.setAdapter(mainAdapter);
-        mainAdapter.notifyDataSetChanged();
+        //Create and populate a list of tutor names
+        String[] names = new String[] {"Alec Brion"};
 
         ArrayList<String> tutorList = new ArrayList<String>();
+        tutorList.addAll( Arrays.asList(names));
 
-        /*itemsAdapter = new ArrayAdapter<String>(
+        //Create ArrayAdapter using tutorList
+        listAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
                 tutorList
-        );*/
+        );
+
+        mainList.setAdapter(listAdapter);
 
     }
 
-    @Override
-    public void onResume(){
-        super.onResume();
-    }
 
 }
